81ab312ec67799ae972b16f95ae3ccc5f38abe05
Raymond Foo <raym.foo83@gmail.com>
2015-11-07 00:38:11 +0800

    Updated with comments
    Updated plot1.R & plot2.R with comments

--- plot1.R
+++ plot1.R
@@ -1,3 +1,10 @@
+# ==================================================
+# Plot 1.R
+# Created by : Raymond Foo
+# Created on : 2015-11-07
+# ==================================================
+
+
 # This function loads the electrical poweer consumption date from file.
 # Date and time are formatted as date and datetime in the fDate and fTime column respectively.
 # Date is subsetted to be between 2007-02-01 and 2007-02-02 inclusively.
@@ -15,7 +22,7 @@ loadData <- function(){
 }
 
 
-#Draw graphs and export as plot1.png
+#This function draws the graph and export as plot1.png
 generateGraph <- function(){
   par(cex=0.75)
   par(mar=c(5.1,6.1,4.1,2.1))
@@ -23,6 +30,7 @@ generateGraph <- function(){
   dev.copy(png, file = "plot1.png")
   dev.off()
 }
+
 
 #run functions
 electricData<-loadData()
